887bddccbb4a81b4018808d8886851724d50631d
YuYeaJin <yyj9290@naver.com>
2024-12-23 11:29:39 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "/src/pages/Home";
+import About from "/src/pages/About";
+import Contact from "/src/pages/Contact";
+import Dashboard from "/src/pages/Dashboard/Dashboard";
+import Header from "/src/components/Header";
+import Navbar from "/src/components/Navbar";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <article>
+        {/* Routing 정보 객체 */}
+        <Routes>
+          {/* 개별 Router 객체 */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </article>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header>
+      <h1>React Routing Example</h1>
+    </header>
+  );
+}
+export default Header;
